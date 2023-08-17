45eb366b65f36de03d9286e836c0f69998b64810
Pedro William 🎮🎶 <axlwilliamrose5@hotmail.com>
2023-08-17 10:42:06 -0300

    Ajustes possibilitando efetuar login com diferentes usuários do postgres; Adicionado LEFT JOIN com estados para mostrar dados da coluna 'UF';

--- AppDatabase/Program.cs
+++ AppDatabase/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Npgsql;
 
+Console.WriteLine("Efetue o login com o usuário e senha do professor.");
+Console.Write("Usuário: ");
+string userId = Console.ReadLine();
+Console.Write("Senha: ");
+string password = GetMaskedPassword();
+Console.WriteLine();
+
 NpgsqlConnection conn = new NpgsqlConnection();
-string con = "Server=aluguelcarros.cxdeckrhjvvc.us-east-1.rds.amazonaws.com;Port=5432;Database=aluguelcarros;User Id=postgres;Password=pedrow2001";
+string con = ($"Server=aluguelcarros.cxdeckrhjvvc.us-east-1.rds.amazonaws.com;Port=5432;Database=aluguelcarros;User Id={userId};Password={password}");
 
 try
 {
@@ -15,9 +22,10 @@ catch
     Console.WriteLine("Erro na conexão com o RDS AWS...");
 }
 
-
 try
 {
+    Console.Clear();
+    Console.WriteLine("Inserção na tabela usuario.");
     Console.Write("Digite o login: ");
     string login = Console.ReadLine();
     Console.Write("Digite a senha: ");
@@ -30,14 +38,17 @@ try
 }
 catch
 {
-    Console.WriteLine("Erro na query!");
-}
+    Console.WriteLine("Erro na inserção!");
+}   
 
-try
-{
-    string select = "SELECT e.idendereco, e.logradouro, e.numero, e.bairro , m.codibge, m.nome FROM rentcar.endereco e LEFT JOIN rentcar.municipio m ON m.codibge = e.codmunicipio";
-    NpgsqlCommand cmd = new NpgsqlCommand(select, conn);
-    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+    Console.Clear();
+    Console.WriteLine("SELECT na tabela endereços com LEFT JOIN nas tabelas municípios e estados.");
+    string select = "SELECT e.idendereco, e.logradouro, e.numero, e.bairro , m.codibge, m.nome, es.uf FROM rentcar.endereco e " +
+    "LEFT JOIN rentcar.municipio m ON m.codibge = e.codmunicipio " +
+    "LEFT JOIN rentcar.estado es ON es.id = m.idestado";
+    NpgsqlCommand cmd1 = new NpgsqlCommand(select, conn);
+    NpgsqlDataReader reader = cmd1.ExecuteReader();
     while (reader.Read())
     {
         Console.WriteLine("Id: " + reader.GetInt32(0));
@@ -46,11 +57,31 @@ try
         Console.WriteLine("Bairro: " + reader.GetString(3));
         Console.WriteLine("Código Município: " + reader.GetInt32(4));
         Console.WriteLine("Município: " + reader.GetString(5));
+        Console.WriteLine("UF: " + reader.GetString(6));
         Console.WriteLine();
     }
-}
-catch
+
+
+static string GetMaskedPassword()
 {
-    Console.WriteLine("Erro na query!");
+    var pass = string.Empty;
+    ConsoleKey key;
+    do
+    {
+        var keyInfo = Console.ReadKey(intercept: true);
+        key = keyInfo.Key;
+
+        if (key == ConsoleKey.Backspace && pass.Length > 0)
+        {
+            Console.Write("\b \b");
+            pass = pass[0..^1];
+        }
+        else if (!char.IsControl(keyInfo.KeyChar))
+        {
+            Console.Write("*");
+            pass += keyInfo.KeyChar;
+        }
+    } while (key != ConsoleKey.Enter);
+    return pass;
 }
 
