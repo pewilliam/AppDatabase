031b0ed9ae61ee051223c015f2306d284b4e0613
Pedro William 🎮🎶 <axlwilliamrose5@hotmail.com>
2023-08-17 15:51:36 -0300

    Adicionado INSERT e SELECT em métodos. Modificado o INSERT para ocultar o input na hora de inserir a senha do usuario da tabela.

--- AppDatabase/Program.cs
+++ AppDatabase/Program.cs
@@ -1,70 +1,86 @@
 ﻿using Npgsql;
 
+string userId, password;
+
 Console.WriteLine("Efetue o login com o usuário e senha do professor.");
 Console.Write("Usuário: ");
-string userId = Console.ReadLine();
+userId = Console.ReadLine();
 Console.Write("Senha: ");
-string password = GetMaskedPassword();
+password = GetMaskedPassword();
 Console.WriteLine();
 
-NpgsqlConnection conn = new NpgsqlConnection();
-string con = ($"Server=aluguelcarros.cxdeckrhjvvc.us-east-1.rds.amazonaws.com;Port=5432;Database=aluguelcarros;User Id={userId};Password={password}");
+string connectionString = $"Server=aluguelcarros.cxdeckrhjvvc.us-east-1.rds.amazonaws.com;Port=5432;Database=aluguelcarros;User Id={userId};Password={password}";
 
-try
+using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
 {
-    conn.ConnectionString = con;
-    conn.Open();
-    Console.WriteLine("Conexão efetuada com RDS AWS! Pressione enter para continuar...");
-    Console.ReadLine();
-}
-catch
-{
-    Console.WriteLine("Erro na conexão com o RDS AWS...");
-}
-
-try
-{
-    Console.Clear();
-    Console.WriteLine("Inserção na tabela usuario.");
-    Console.Write("Digite o login: ");
-    string login = Console.ReadLine();
-    Console.Write("Digite a senha: ");
-    string senha = Console.ReadLine();
-    string insert = ($"INSERT INTO rentcar.usuario(login, senha) VALUES ('{login}', '{senha}')");
-    NpgsqlCommand cmd = new NpgsqlCommand(insert, conn);
-    cmd.ExecuteNonQuery();
-    Console.WriteLine("Query efetuada com sucesso! Pressione enter para continuar para o SELECT...");
-    Console.ReadLine();
-}
-catch
-{
-    Console.WriteLine("Erro na inserção!");
-}
-try
-{
-    Console.Clear();
-    Console.WriteLine("SELECT na tabela endereços com LEFT JOIN nas tabelas municípios e estados.");
-    string select = "SELECT e.idendereco, e.logradouro, e.numero, e.bairro , m.codibge, m.nome, es.uf FROM rentcar.endereco e " +
-    "LEFT JOIN rentcar.municipio m ON m.codibge = e.codmunicipio " +
-    "LEFT JOIN rentcar.estado es ON es.id = m.idestado";
-    NpgsqlCommand cmd1 = new NpgsqlCommand(select, conn);
-    NpgsqlDataReader reader = cmd1.ExecuteReader();
-    while (reader.Read())
+    try
     {
-        Console.WriteLine("Id: " + reader.GetInt32(0));
-        Console.WriteLine("Logradouro: " + reader.GetString(1));
-        Console.WriteLine("Número: " + reader.GetString(2));
-        Console.WriteLine("Bairro: " + reader.GetString(3));
-        Console.WriteLine("Código Município: " + reader.GetInt32(4));
-        Console.WriteLine("Município: " + reader.GetString(5));
-        Console.WriteLine("UF: " + reader.GetString(6));
-        Console.WriteLine("---------------------------------");
+        conn.Open();
+        Console.WriteLine("Conexão efetuada com RDS AWS! Pressione enter para continuar...");
+        Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Erro na conexão com o RDS AWS: " + ex.Message);
     }
 
+    InsertIntoUsuario(conn);
+    PerformSelect(conn);
 }
-catch
+
+static void InsertIntoUsuario(NpgsqlConnection connection)
 {
-    Console.WriteLine("Erro no SELECT!");
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("Inserção na tabela usuario.");
+        Console.Write("Digite o login: ");
+        string login = Console.ReadLine();
+        Console.Write("Digite a senha: ");
+        string senha = GetMaskedPassword();
+
+        string insertQuery = "INSERT INTO rentcar.usuario(login, senha) VALUES (@login, @senha)";
+        using NpgsqlCommand cmd = new NpgsqlCommand(insertQuery, connection);
+
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Query efetuada com sucesso! Pressione enter para continuar para o SELECT...");
+        Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Erro na inserção: " + ex.Message);
+    }
+}
+
+static void PerformSelect(NpgsqlConnection connection)
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("SELECT na tabela endereços com LEFT JOIN nas tabelas municípios e estados.");
+        string selectQuery = "SELECT e.idendereco, e.logradouro, e.numero, e.bairro, m.codibge, m.nome, es.uf FROM rentcar.endereco e " +
+            "LEFT JOIN rentcar.municipio m ON m.codibge = e.codmunicipio " +
+            "LEFT JOIN rentcar.estado es ON es.id = m.idestado";
+
+        using NpgsqlCommand cmd = new NpgsqlCommand(selectQuery, connection);
+        using NpgsqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine("Id: " + reader.GetInt32(0));
+            Console.WriteLine("Logradouro: " + reader.GetString(1));
+            Console.WriteLine("Número: " + reader.GetString(2));
+            Console.WriteLine("Bairro: " + reader.GetString(3));
+            Console.WriteLine("Código Município: " + reader.GetInt32(4));
+            Console.WriteLine("Município: " + reader.GetString(5));
+            Console.WriteLine("UF: " + reader.GetString(6));
+            Console.WriteLine("---------------------------------");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Erro no SELECT: " + ex.Message);
+    }
 }
 
 
